1dd4b5a998156a0bc0094340ce1fcc383a193996
linasmk <linas.mackonis@gmail.com>
2020-07-14 15:13:30 +0200

    Stage 2: Implementing Redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import React from "react";
-
 const Header = props => {
-  return (
-    <header>
+	return (<header>
       <h1 className="main-heading">{props.title}</h1>
       <h2>{props.handleCopyrightDate}</h2>
-    </header>
-  );
+    </header>);
 };
 Header.defaultProps = {
-  title: "Personal Digital Library"
+	title: "Personal Digital Library"
 };
-
 export default Header;
